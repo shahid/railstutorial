d016ffc6eed736c197a35ea49b9bd098546c45e0
Shahid PA <shahid.azeez@gmail.com>
2015-05-22 19:35:18 +0530

    Finish log in/log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
-  
+
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
@@ -13,17 +13,17 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
-  
+
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
   end
-  
+
   test "email should be present" do
     @user.email = "     "
     assert_not @user.valid?
   end
-  
+
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -33,7 +33,7 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-  
+
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
@@ -42,7 +42,7 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
-  
+
   test "email should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
@@ -50,5 +50,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
 end
